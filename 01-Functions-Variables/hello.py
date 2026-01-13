4912046b6d25d221222af68a7ef7b5011d74b327
manuelpaez04 <manuel.p@blockchanger.io>
2026-01-13 09:11:15 -0400

    13-01-2026

---
+++ 01-Functions-Variables/hello.py
@@ -0,0 +1,13 @@
+# Variables and Functions
+
+#name = input("What's your name? ")
+#print("Hello", {name})
+
+# As user for their name
+name = input("What's your name? ")
+
+# Remove whitespace from str
+name = name.strip()
+
+# Say hello to user
+print(f"Hello, {name}")
